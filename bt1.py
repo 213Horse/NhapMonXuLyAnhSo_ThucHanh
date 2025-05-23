31c628f1749561720d3d69aa8b7fe13b45c2dcd8
213Horse <minhquando59@gmail.com>
2025-05-23 07:43:21 +0700

    pussh code

---
+++ bt1.py
@@ -0,0 +1,12 @@
+from PIL import Image
+import numpy as np
+img = Image.open("bird.png")
+img.show()
+
+
+
+
+
+
+
+
---
+++ bt2.py
@@ -0,0 +1,12 @@
+import numpy as np
+import imageio.v2 as iio
+import matplotlib.pyplot as plt
+data = iio.imread("bird.png")
+plt.imshow(data)
+plt.show()
+
+
+
+
+
+
